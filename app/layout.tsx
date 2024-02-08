c311122456a471938cc2f18ddbf145ee20987a0a
emvk09 <edwinmoncy@gmail.com>
2024-02-08 10:40:03 +0530

    Feat: Add fonts file and  update packages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,73 @@
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
-import { PropsWithChildren } from "react";
 import "./globals.css";
+import { PropsWithChildren } from "react";
 import { cn } from "@/lib/utils";
-
-const popins = Poppins({
-  weight: "600",
-  subsets: ["latin"],
-});
+import { poppins } from "@/lib/fonts";
 
 export const metadata: Metadata = {
-  title: "CITA | Home",
-  description: "Conversational Interview and Training Application",
+  // inorder for the "title.template" to work, the pages in that specific route must also have a "title" in its metadata.
+  // if the pages in that specific route have no "title", then "title.default" of the closest parent Layout will be shown for that particular page
+  title: { default: "CITA", template: "%s | CITA" },
+  description: "Conversational Interview and Training Assistant",
+  keywords: [
+    "talking",
+    "ai",
+    "talkingPDF",
+    "pdf",
+    "free",
+    "chatbot",
+    "scrapper",
+    "easy",
+  ],
+  generator: "Next.js",
+  applicationName: "CITA",
+  referrer: "origin-when-cross-origin",
+  authors: [{ name: "" }],
+  creator: "",
+  publisher: "",
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  metadataBase: new URL(`http://localhost:3000`),
+  alternates: {
+    canonical: "/",
+    languages: {
+      "en-US": "/en-US",
+    },
+  },
+  openGraph: {
+    title: "talkingPDF",
+    description: "Talk with your pdf for free",
+    url: `http://localhost:3000`,
+    siteName: "talkingPDF",
+    // images: [
+    //   {
+    //     url: "/images/logo-background.jpg",
+    //     width: 800,
+    //     height: 600,
+    //   },
+    // ],
+    locale: "en_US",
+    type: "website",
+  },
+  icons: {
+    // icon: "/images/logo-background.jpg",
+  },
+  category: "interview virtual ai chat bot free",
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 };
 
 export default function RootLayout({ children }: PropsWithChildren) {
@@ -22,8 +78,8 @@ export default function RootLayout({ children }: PropsWithChildren) {
       </head>
       <body
         className={cn(
-          "min-h-screen font-sans antialiased grainy",
-          popins.className
+          "min-h-screen font-sans antialiased bg-white",
+          poppins.className
         )}
       >
         {children}
---
+++ components/Navbar.tsx
@@ -0,0 +1,83 @@
+import Link from "next/link";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import { buttonVariants } from "./ui/button";
+import { ArrowRight } from "lucide-react";
+import { opensans, pacifico } from "@/lib/fonts";
+import Image from "next/image";
+
+export default function Navbar() {
+  return (
+    <nav className="w-full h-16 sticky z-30 inset-x-0 top-0 border-b border-gray-200 bg-white/70 backdrop-blur-xl transition-all">
+      <MaxWidthWrapper>
+        <div className="flex h-16 items-center justify-between">
+          <Link
+            href="/"
+            className="flex items-center font-semibold text-xl select-none xs:space-x-2 space-x-1"
+          >
+            <Image
+              alt="logo"
+              src="/images/logo.png"
+              width={33}
+              height={33}
+              quality={100}
+              priority
+              className="w-auto h-auto"
+            />
+            <span className={`${opensans.className} text-[#f658ee] text-2xl`}>
+              C
+            </span>
+            <span className={`${pacifico.className} text-[#5a338a]`}>I</span>
+            <span className={`${opensans.className} text-[#f658ee] text-2xl`}>
+              T
+            </span>
+            <span className={`${pacifico.className} text-[#5a338a]`}>A.</span>
+          </Link>
+
+          {/* Add mobile navbar */}
+
+          <div className="hidden sm:flex items-center space-x-4">
+            <Link
+              href="/pricing"
+              className={buttonVariants({
+                variant: "ghost",
+                size: "sm",
+                className: "text-[15px] text-gray-900 font-semibold",
+              })}
+            >
+              <h4>Pricing</h4>
+            </Link>
+            <Link
+              href="/"
+              className={buttonVariants({
+                variant: "ghost",
+                size: "sm",
+                className: "text-[15px] text-gray-900 font-semibold",
+              })}
+            >
+              <h4>Sign in</h4>
+            </Link>
+            <Link
+              href="/"
+              className={buttonVariants({
+                variant: "ghost",
+                size: "sm",
+                className: "text-[15px] text-gray-900 font-semibold",
+              })}
+            >
+              <h4>Sign out</h4>
+            </Link>
+            <Link
+              href="/"
+              className={buttonVariants({
+                variant: "default",
+                size: "sm",
+              })}
+            >
+              Get started <ArrowRight className="ml-1.5 h-5 w-5" />
+            </Link>
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </nav>
+  );
+}
